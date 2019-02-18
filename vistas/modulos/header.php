51a7c6b7ac6db2a98ccd7ef7278cde01a50ba0f7
OperacionesVexom <operaciones.vexom@gmail.com>
2019-02-18 12:38:23 -0600

    Proyecto en MVC

---
+++ vistas/modulos/header.php
@@ -0,0 +1,44 @@
+<div class="navbar-dark fixed-top">
+	<nav class="navbar navbar-expand-md bg-dark container">
+		<a class="navbar-brand" href="index.html">
+			<!-- <img src="img/logo.png"> -->
+			HOLA
+		</a>
+		
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav ml-auto mr-5">
+				<li class="nav-item active">
+					<a class="nav-link" href="#inicio">Inicio <span class="sr-only">(current)</span></a>
+				</li>
+	
+				<li class="nav-item">
+					<a class="nav-link" href="#">Nosotros</a>
+				</li>
+	
+				<li class="nav-item">
+					<a class="nav-link" href="#">Contacto</a>
+				</li>
+			</ul>
+
+			<span class="navbar-text text-white">
+				<ul class="navbar-nav ml-auto mr-5">
+					<li class="nav-item">
+						<a class="nav-link" href="#modalIngreso" data-toggle="modal">Ingresa</a>
+					</li>
+		
+					<li class="nav-item active">
+						<a class="nav-link" href="#">|</a>
+					</li>
+		
+					<li class="nav-item">
+						<a class="nav-link" href="#modalRegistro" data-toggle="modal">Regístrate</a>
+					</li>
+				</ul>
+			</span>
+		</div>
+	</nav>
+</div>
